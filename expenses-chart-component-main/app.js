7a1b51afcb627dbdc6ee23c357f048471315895e
karine <110117197+karine-laurindo@users.noreply.github.com>
2023-05-05 17:09:49 -0300

    Add files via upload

---
+++ expenses-chart-component-main/app.js
@@ -0,0 +1,34 @@
+document.querySelector('.coluna1').addEventListener('mouseover',()=>{
+    document.querySelector('.coluna1').style.background = 'hsl(186, 34%, 60%)';
+    document.querySelector('.value1').textContent = '$17.45'
+})
+
+document.querySelector('.coluna2').addEventListener('mouseover',()=>{
+    document.querySelector('.coluna2').style.background = 'hsl(186, 34%, 60%)';
+    document.querySelector('.value2').textContent = '$34.91'
+})
+
+document.querySelector('.coluna3').addEventListener('mouseover',()=>{
+    document.querySelector('.coluna3').style.background = 'hsl(186, 34%, 60%)';
+    document.querySelector('.value3').textContent = '$52.36'
+})
+
+document.querySelector('.coluna4').addEventListener('mouseover',()=>{
+    document.querySelector('.coluna4').style.background = 'hsl(186, 34%, 60%)';
+    document.querySelector('.value4').textContent = '$31.07'
+})
+
+document.querySelector('.coluna5').addEventListener('mouseover',()=>{
+    document.querySelector('.coluna5').style.background = 'hsl(186, 34%, 60%)';
+    document.querySelector('.value5').textContent = '$23.39'
+})
+
+document.querySelector('.coluna6').addEventListener('mouseover',()=>{
+    document.querySelector('.coluna6').style.background = 'hsl(186, 34%, 60%)';
+    document.querySelector('.value6').textContent = '$43.28'
+})
+
+document.querySelector('.coluna7').addEventListener('mouseover',()=>{
+    document.querySelector('.coluna7').style.background = 'hsl(186, 34%, 60%)';
+    document.querySelector('.value7').textContent = '$25.48'
+})
